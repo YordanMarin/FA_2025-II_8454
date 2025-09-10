8ce2e53731db99cac2b3a78835b4bef3a0a77e72
Yordan <yordan@gmail.com>
2025-09-10 12:17:44 -0500

    subiendo hasta el ejer5 sem3

--- SEMANA1_C#/Program.cs
+++ SEMANA1_C#/Program.cs
@@ -10,7 +10,7 @@ namespace SEMANA1_C_
     {
         static void Main(string[] args)
         {
-            ejer1();
+            ejer5();
             Console.ReadKey();
         }
 
@@ -29,21 +29,51 @@ namespace SEMANA1_C_
 
         static void ejer2()
         {
-
+            Console.Write("\"Yordan\"");
         }
 
         static void ejer3()
         {
+            Console.Write("Ingrese número 1: ");
+            int num1 = int.Parse(Console.ReadLine());
 
+            Console.Write("Ingrese número 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            double divi = (double)num1/(double)num2;
+
+            Console.WriteLine("\nSuma: "+(num1+num2));
+            Console.WriteLine("Resta: " + (num1 - num2));
+            Console.WriteLine("Multiplicación: " + (num1 * num2));
+            Console.WriteLine("División: " + divi);
         }
 
         static void ejer4()
         {
+            Console.Write("Ingrese un número decimal: ");
+            double num = double.Parse(Console.ReadLine());
 
+            double raiz2 = Math.Sqrt(num);
+            double redo = Math.Round(num,0);
+            double cubo = Math.Pow(num, 3);
+            double raiz3 = Math.Pow(num, 1/3d);
+
+            Console.WriteLine("\nRaiz 2: "+raiz2);
+            Console.WriteLine("Redondeado: " + redo);
+            Console.WriteLine("Al cubo: " + cubo);
+            Console.WriteLine("Raiz cubica: " + raiz3);
         }
 
         static void ejer5()
         {
+            Console.Write("Ingrese un número: ");
+            string num = Console.ReadLine();
+
+            int entero = int.Parse(num);
+            double deci = double.Parse(num);
+
+            Console.WriteLine("\nResto: " + (entero % 2));
+            Console.WriteLine("Dividido 3: " + (deci / 3));
 
         }
 
--- SEMANA1_PYTHON/SEMANA1_PYTHON.py
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -4,4 +4,40 @@ def ejer1():
 
     print(f"\n{nombre}, bievenido a FA de {carrera}")
 
-ejer1()
+def ejer2():
+    print("\"Yordan\"")
+
+def ejer3():
+    num1 = int(input("Ingrese número 1: "))
+    num2 = int(input("Ingrese número 2: "))
+
+    print("\nSuma: ", (num1+num2))
+    print("Resta: ", (num1-num2))
+    print("Multiplicación: ", (num1*num2))
+    print("División: ", (num1/num2))
+
+import math #importando la libreria math
+
+def ejer4():
+    num = float(input("Ingrese un número decimal: "))
+
+    raiz2 = math.sqrt(num)
+    redo = round(num,0)
+    cubo = math.pow(num,3)
+    raiz3 = math.pow(num,1/3)
+
+    print("\nRaiz 2: ",raiz2)
+    print("Redondeado: ", redo)
+    print("Al cubo: ", cubo)
+    print("Raiz 3: ", raiz3)
+
+def ejer5():
+    num = input("Ingrese un número: ")
+
+    entero = int(num)
+    deci = float(num)
+
+    print("\nResto: ", entero%2)
+    print("Dividido 3: ", deci/3)
+
+ejer5()
