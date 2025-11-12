156224225935370070988705ab9063cec25a24e2
yordan <yordanmarin@gmail.com>
2025-11-12 12:17:49 -0500

    menu interactivo

---
+++ SEMANA13/Estudiantes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA13
+{
+    internal class Estudiantes
+    {
+        string[] nombres = new string[0];
+        byte[] edades = new byte[0];
+        int pos = 0;
+
+        public int buscar(string nom)
+        {
+            for(int i = 0; i < nombres.Length; i++)
+            {
+                if(nom == nombres[i])  return i;
+            }
+            return -1;
+        }
+
+        public void modificar()
+        {
+            Console.Write("Ingrese el nombre a modicar: ");
+            string nom = Console.ReadLine();
+
+            int indice = buscar(nom);
+
+            if (indice != -1)
+            {
+                Console.Write("Ingrese su nuevo nombre: ");
+                nombres[indice] = Console.ReadLine();
+                Console.Write("Ingrese su nueva edad ");
+                edades[indice] = byte.Parse(Console.ReadLine());
+            }
+            else Console.WriteLine("No existe");
+        }
+
+        public void eliminar()
+        {
+            Console.Write("Ingrese el nombre a eliminar: ");
+            string nom = Console.ReadLine();
+
+            int indice = buscar(nom);
+
+            if (indice != -1)
+            {
+                for(int i = indice;i < nombres.Length-1; i++)
+                {
+                    nombres[i] = nombres[i+1];
+                    edades[i] = edades[i+1];
+                }
+                Array.Resize(ref edades, nombres.Length-1);
+                Array.Resize(ref nombres, nombres.Length-1);
+                pos--;
+            }
+            else Console.WriteLine("No existe");
+        }
+    }
+}
---
+++ SEMANA13/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menu = { "INSERTAR", "REGISTRAR", "MODIFICAR", "ELIMINAR", "ORDENAR", "SALIR" };
+            int indice = 0;
+            ConsoleKey tecla;
+            string conti;
+            do
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("************************************************************************************************");
+                    Console.WriteLine("*                             BIENVENIDOS AL SISTEMA DE ESTUDIANTES                            *");
+                    Console.WriteLine("************************************************************************************************");
+                    Console.ResetColor();
+                    Console.WriteLine();
+
+                    for (int i = 0; i < menu.Length; i++)
+                    {
+                        if (indice == i)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write("    " + menu[i] + "    ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write("    " + menu[i] + "    ");
+                        }
+                    }
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------");
+
+                    ConsoleKeyInfo info = Console.ReadKey();
+                    tecla = info.Key;
+
+                    if (tecla == ConsoleKey.RightArrow)
+                    {
+                        indice++;
+                        if (indice > menu.Length - 1)
+                        {
+                            indice = 0;
+                        }
+                    }
+                    else if (tecla == ConsoleKey.LeftArrow)
+                    {
+                        indice--;
+                        if (indice < 0)
+                        {
+                            indice = menu.Length - 1;
+                        }
+                    }
+                } while (tecla != ConsoleKey.Enter);
+
+
+                switch (indice)
+                {
+                    case 0: Console.WriteLine("hola"); break;
+                    case 1: Console.WriteLine("hola1"); break;
+                    case 2: Console.WriteLine("hola2"); break;
+                    case 3: Console.WriteLine("hol3"); break;
+                    case 4: Console.WriteLine("hol4"); break;
+                    case 5: return;
+                }
+
+                while (true)
+                {
+                    Console.Write("\n¿Desea regresar al menú? (S/N)");
+                    conti = Console.ReadLine().ToLower().Trim();
+                    if (conti == "s" || conti == "n") break;
+                    else Console.WriteLine("Opción invalida solo se permiten 's' o 'n'\n");
+                }
+            } while (conti == "s");
+                    Console.ReadKey();
+        }
+    }
+}
